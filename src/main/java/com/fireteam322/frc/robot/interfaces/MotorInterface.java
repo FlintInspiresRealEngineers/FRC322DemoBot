9523fff3344b1ac50ddf00a7757a4c9de6afe626
DESKTOP-OHVI0BO\Ashton <ashton.bradley45274@gmail.com>
2022-03-06 22:37:48 -0500

    Added the functions and basic interface for motors

--- src/main/java/com/fireteam322/frc/robot/interfaces/MotorInterface.java
+++ src/main/java/com/fireteam322/frc/robot/interfaces/MotorInterface.java
@@ -3,7 +3,7 @@ package com.fireteam322.frc.robot.interfaces;
 public interface MotorInterface {
     public void run();
 
-    public void run(int speed);
+    public void run(double speed);
 
     public void stop();
 }
--- src/main/java/com/fireteam322/frc/robot/subsystems/HookClimbingSubSystem.java
+++ src/main/java/com/fireteam322/frc/robot/subsystems/HookClimbingSubSystem.java
@@ -1,6 +1,7 @@
 package com.fireteam322.frc.robot.subsystems;
 
 import com.fireteam322.frc.robot.Constants;
+import com.fireteam322.frc.robot.interfaces.MotorInterface;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -15,10 +16,39 @@ public class HookClimbingSubSystem extends SubsystemBase implements MotorInterfa
     public HookClimbingSubSystem() {
         super();
         this.setName("HookClimb_SubSystem");
+        // TODO We have to figure out which motors need to be inverted cause they are on
+        // oppsite sides.
+
     }
 
     @Override
     public void periodic() {
+
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+    }
+
+    public void runBackHooks(double speed) {
+        rearHookMotorGroup.set(speed);
+    }
+
+    public void runFrontHooks(double speed) {
+        frontHook.set(speed);
+    }
+
+    @Override
+    public void run(double speed) {
+        // TODO Auto-generated method stub
+        rearHookMotorGroup.set(speed);
+
+    }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
 
     }
 }
